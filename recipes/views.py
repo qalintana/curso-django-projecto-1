5998e4add096cd5afef293ed641d48c828485d70
Queiroz Santana <alintana18@gmail.com>
2022-12-20 22:24:21 +0100

    partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html', status=200, context={'name': 'Queiroz Santana'})
+    return render(request, 'recipes/pages/home.html', status=200, context={'name': 'Santana'})
