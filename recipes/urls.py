779982a09589674e621da52f1eb79af002cc2239
Queiroz Santana <alintana18@gmail.com>
2022-12-20 20:01:10 +0100

    removendo sobre e contato

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', status=200, context={'name': 'Queiroz Santana'})
-
-
-def contato(request):
-    return HttpResponse('CONTACTO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
