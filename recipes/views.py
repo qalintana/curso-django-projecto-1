2c79730c4b841ce53991511e0a29f5119e381b29
Queiroz Santana <alintana18@gmail.com>
2022-12-28 08:36:10 +0100

    not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from __localcode.main import make_recipe
+from recipes.models import Recipe
+
 
 # Create your views here.
-
-
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', status=200,
-                  context={'recipes': [make_recipe() for _ in range(10)]})
+                  context={'recipes': recipes})
 
 
-def recipe(request, id):
+def category(request: HttpRequest, category_id: int) -> HttpResponse:
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True)
+
+    # if not recipes:
+    #     raise Http404('Not found 😢')
+    # category_name = getattr(
+    #     getattr(recipes.first(), 'category', None), 'name', 'Not found')
+
+    # recipes = get_list_or_404(
+    #     Recipe, category__id=category_id, is_published=True)
+    # print(recipes)
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', status=200,
+                  context={'recipes': recipes, 'title': f'{recipes[0].category.name} - Category'})
+
+
+def recipe(request: HttpRequest, id: int) -> HttpResponse:
     return render(request, 'recipes/pages/recipe-view.html',
                   status=200, context={'recipe': make_recipe(), 'is_detail_page': True})
